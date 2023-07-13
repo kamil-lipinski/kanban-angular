6f685d6eb12445de8aee2267bb7d5555adddf273
unknown <kamil.lipinski.2000@gmail.com>
2023-07-13 15:21:23 +0200

    aaaa

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBfGMLopt28B6o8XSogz384EWsF8RD5xYw",
+    authDomain: "kanbanfire-94cdf.firebaseapp.com",
+    projectId: "kanbanfire-94cdf",
+    storageBucket: "kanbanfire-94cdf.appspot.com",
+    messagingSenderId: "603974916228",
+    appId: "1:603974916228:web:0a105a4dacb99811ff4ad1",
+    measurementId: "G-ERVLC96MJJ"
+  },
+};
