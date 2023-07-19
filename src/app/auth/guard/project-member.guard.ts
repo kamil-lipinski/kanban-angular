b18bc65dd58fb42203aac45d1f293c1a53d90df8
Kamil <kamil.lipinski.2000@gmail.com>
2023-07-19 22:34:30 +0200

    file format

--- src/app/auth/guard/project-member.guard.ts
+++ src/app/auth/guard/project-member.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Firestore, doc, getDoc } from '@angular/fire/firestore'; 
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
@@ -18,9 +18,9 @@ export class ProjectMemberGuard implements CanActivate {
     const projectId = next.params['projectId'];
     const user = JSON.parse(localStorage.getItem('user')!);
 
-    if(this.authService.isLoggedIn !== true) {
-        this.router.navigate(['sign-in'])
-        return false
+    if (this.authService.isLoggedIn !== true) {
+      this.router.navigate(['sign-in'])
+      return false
     }
 
     return this.isMemberOfProject(projectId, user.uid);
@@ -30,24 +30,24 @@ export class ProjectMemberGuard implements CanActivate {
     const projectRef = doc(this.store, 'projects', projectId);
 
     try {
-        const projectSnapshot = await getDoc(projectRef);
-        if (projectSnapshot.exists()) {
-          const projectData = projectSnapshot.data();
-          const members = projectData?.['members'] || {};
-  
-          if (Object.prototype.hasOwnProperty.call(members, uid)) {
-            return true;
-          } else {
-            this.router.navigate(['404-not-found']);
-            return false;
-          }
+      const projectSnapshot = await getDoc(projectRef);
+      if (projectSnapshot.exists()) {
+        const projectData = projectSnapshot.data();
+        const members = projectData?.['members'] || {};
+
+        if (Object.prototype.hasOwnProperty.call(members, uid)) {
+          return true;
         } else {
           this.router.navigate(['404-not-found']);
           return false;
         }
-      } catch (error) {
-        console.error('Error fetching project data:', error);
+      } else {
+        this.router.navigate(['404-not-found']);
         return false;
       }
+    } catch (error) {
+      console.error('Error fetching project data:', error);
+      return false;
     }
+  }
 }
