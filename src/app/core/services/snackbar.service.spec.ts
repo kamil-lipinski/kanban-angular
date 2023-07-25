efeb3cf8a56dc5438ed33d3438019bdd6c5c02b3
Kamil Lipiński <kamil.lipinski.2000@gmail.com>
2023-07-25 14:58:56 +0200

    update

--- src/app/core/services/snackbar.service.spec.ts
+++ src/app/core/services/snackbar.service.spec.ts
@@ -1,16 +1,55 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { SnackbarService } from './snackbar.service';
 
-describe('Service: Snackbar', () => {
+fdescribe('SnackbarService', () => {
+  let snackbarService: SnackbarService;
+  let mockSnackBar: any;
+
   beforeEach(() => {
+    mockSnackBar = jasmine.createSpyObj('MatSnackBar', ['open']);
+
     TestBed.configureTestingModule({
-      providers: [SnackbarService]
+      imports: [MatSnackBarModule],
+      providers: [
+        { provide: MatSnackBar, useValue: mockSnackBar },
+        SnackbarService],
     });
+
+    snackbarService = TestBed.inject(SnackbarService);
   });
 
-  it('should ...', inject([SnackbarService], (service: SnackbarService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    expect(snackbarService).toBeTruthy();
+  });
+
+  it('should call MatSnackBar.open with error message', () => {
+    const errorMessage = 'This is an error message';
+    snackbarService.errorSnackbar(errorMessage);
+    expect(mockSnackBar.open).toHaveBeenCalledWith(
+      errorMessage,
+      'Zamknij',
+      jasmine.objectContaining({
+        panelClass: ['error-snackbar'],
+        verticalPosition: 'top',
+        horizontalPosition: 'end',
+        duration: 3000,
+      })
+    );
+  });
+
+  it('should call MatSnackBar.open with success message', () => {
+    const successMessage = 'This is a success message';
+    snackbarService.successSnackbar(successMessage);
+    expect(mockSnackBar.open).toHaveBeenCalledWith(
+      successMessage,
+      'Zamknij',
+      jasmine.objectContaining({
+        panelClass: ['succes-snackbar'],
+        verticalPosition: 'top',
+        horizontalPosition: 'end',
+        duration: 3000,
+      })
+    );
+  });
 });
